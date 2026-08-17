5c131e4f29b36f421468605e96523c6e7bfe735e
raty <ratygore@gmail.com>
2026-08-17 22:56:36 +0200

    enable live content editing for editors

--- src/app/api/draft-mode/enable/route.ts
+++ src/app/api/draft-mode/enable/route.ts
@@ -2,7 +2,7 @@
  * This file is used to allow Presentation to set the app in Draft Mode, which will load Visual Editing
  * and query draft content and preview the content as it will appear once everything is published.
  */
-import { defineEnabledDraftMode } from 'next-sanity/draft-mode'
+import { defineEnableDraftMode } from 'next-sanity/draft-mode'
 import { client } from '@/sanity/lib/client'
 import { token } from '@/sanity/lib/token'
 
